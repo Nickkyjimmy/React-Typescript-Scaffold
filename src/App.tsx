2d2104e10cfe9a0b08fa3b7588d63b4a6a55943b
sontungN <s3979348@rmit.edu.vn>
2025-05-08 15:27:52 +0700

    feat(routes): add login page and protected route for dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,28 @@ import HomePage from "./pages/home";
 import AboutPage from "./pages/about";
 import DashboardPage from "./pages/dashboard";
 import NotFoundPage from "./pages/not-found";
+import { LoginPage } from "./pages/login";
+import ProtectedRoute from "./components/common/ProtectedRoute";
+import { UserRole } from "./types/user-role";
 
 const App = () => {
   return (
-      <Routes>
-        <Route path="/" element={<MainLayout />}>
-          <Route index element={<HomePage />} />
-          <Route path="about" element={<AboutPage />} />
-          <Route path="dashboard" element={<DashboardPage />} />
-          <Route path="*" element={<NotFoundPage />} />
-        </Route>
-      </Routes>
+    <Routes>
+      <Route path="/" element={<MainLayout />}>
+        <Route index element={<HomePage />} />
+        <Route path="about" element={<AboutPage />} />
+        <Route
+          path="dashboard"
+          element={
+            <ProtectedRoute accessRole={[UserRole.ADMIN]}>
+              <DashboardPage />
+            </ProtectedRoute>
+          }
+        />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="*" element={<NotFoundPage />} />
+      </Route>
+    </Routes>
   );
 };
 
